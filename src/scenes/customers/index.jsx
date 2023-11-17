7560cae473fcce6e03a8c8b59c55587d4540dad6
sumasri832 <kethasumasri@gmail.com>
2023-11-17 10:32:11 +0530

    added jenkins file

--- src/scenes/customers/index.jsx
+++ src/scenes/customers/index.jsx
@@ -1,4 +1,4 @@
-import { Box, useTheme } from "@mui/material";
+import { Box, useTheme,useMediaQuery } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { tokens } from "../../theme2";
 import { mockDataTeam } from "../../mock_data/customersData";
